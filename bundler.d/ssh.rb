0ee7df56f3034522821baeca28b2385dd10adbcb
Marius Vollmer <mvollmer@redhat.com>
2018-10-26 14:33:02 +0300

    HACKING - configure for devel

---
+++ bundler.d/ssh.rb
@@ -0,0 +1,6 @@
+gem 'smart_proxy_dynflow', :path => '../smart_proxy_dynflow'
+gem 'smart_proxy_dynflow_core', :path => '../smart_proxy_dynflow'
+gem 'smart_proxy_remote_execution_ssh', :path => '../smart_proxy_remote_execution_ssh'
+gem 'foreman_remote_execution_core', :path => '../foreman_remote_execution'
+gem 'ed25519'
+gem 'bcrypt_pbkdf'
